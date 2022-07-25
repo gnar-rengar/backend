92e9031fd9d90f4172dd3c46f07a6ea04e791745
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-07-25 14:25:25 +0900

    [fix] 쿠키 설정 변경

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(
         saveUninitialized: false,
         secret: process.env.COOKIE_SECRET,
         cookie: {
+            domain: '.localhost:3000',
             httpOnly: false,
             SameSite: 'none',
             secure: false,
