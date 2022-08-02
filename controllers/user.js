268fa87e45835a881eabf27c6812b6890ab9de12
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-02 17:48:07 +0900

    [fix] mypage 수정

--- controllers/user.js
+++ controllers/user.js
@@ -366,9 +366,15 @@ async function mypage(req, res) {
         }
     } catch (error) {
         console.log(error)
-        res.json({
-            message: '내정보 불러오기에 실패하였습니다.',
-        })
+        if (error == '로그인이 필요합니다.') {
+            return res.status(401).json({
+                message: '로그인이 필요합니다.',
+            })
+        } else {
+            res.json({
+                message: '내정보 불러오기에 실패하였습니다.',
+            })
+        }
     }
 }
 
