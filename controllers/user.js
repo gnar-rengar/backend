dcf02483979562632cc95e4cdad12b98787abbd7
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-03 01:57:16 +0900

    [fix] mypage 수정

--- controllers/user.js
+++ controllers/user.js
@@ -315,7 +315,6 @@ async function recentRecord(req, res) {
 
 async function mypage(req, res) {
     const userId = res.locals.userId
-    console.log(userId)
 
     if (!userId) {
         return res.status(401).json({
