c392545f6e4f48c0eca851b9ef345d4b016a8833
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-04 17:10:55 +0900

    [fix] cookie domain 수정

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const RefreshToken = require('../schemas/refreshToken')
 require('dotenv').config()
 
 const COOKIE_OPTIONS = {
-    domain: '.duo-duo.ga',
+    domain: 'http://duo-duo.ga',
     // httpOnly: false,
     sameSite: 'none',
     secure: true,
