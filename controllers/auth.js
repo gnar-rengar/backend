a072bbf3d8deb6fadce945b4198d8885b88d856e
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-04 17:15:03 +0900

    [fix] cookie domain 수정

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const tokenExpireTime = process.env.VALID_ACCESS_TOKEN_TIME
 const rtokenExpireTime = process.env.VALID_REFRESH_TOKEN_TIME
 
 const COOKIE_OPTIONS = {
-    domain: '.duo-duo.ga',
+    domain: 'duo-duo.ga',
     // httpOnly: false,
     sameSite: 'none',
     secure: false,
