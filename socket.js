5d19348efb62b36cdf7d3e01d02af16faa04ad71
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-06 16:08:09 +0900

    [fix] cors option 수정

--- socket.js
+++ socket.js
@@ -28,6 +28,7 @@ const io = require('socket.io')(server, {
     cors: {
         origin: [
             'http://localhost:3000',
+            'http://localhost:3001',
             'http://duo-duo.ga'
         ],
         credentials: true,
