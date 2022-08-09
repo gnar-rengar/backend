d393d4fd9930b69400f10df0fccee78596e8c6d6
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-09 18:33:07 +0900

    [fix] cookie option 수정

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const RefreshToken = require('../schemas/refreshToken')
 require('dotenv').config()
 
 const COOKIE_OPTIONS = {
-    domain: '.duoduo.lol',
+    domain: 'https://duoduo.lol',
     httpOnly: true,
     sameSite: 'none',
     secure: true
