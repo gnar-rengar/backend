6108bccbfb4048d2c284457ffcb5effafc13d88f
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-16 01:07:14 +0900

    [fix] test router 삭제

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router()
 
-const test = require('./test')
 const auth = require('./auth')
 const chat = require('./chat')
 const duo = require('./duo')
 const onboarding = require('./onboarding')
 const user = require('./user')
 
-router.use('/test', test)
 router.use('/auth', auth)
 router.use('/chat', chat)
 router.use('/duo', duo)
