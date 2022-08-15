98d43d59927802ee04f87770f434c163c348b50e
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-16 01:06:56 +0900

    [fix] test router 삭제

--- routes/test.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const testController = require('../controllers/test')
-const multer = require('../middlewares/multers/multer')
-const { checkTokens } = require('../middlewares/auth')
-
-router.get('/', testController.test)
-
-module.exports = router
