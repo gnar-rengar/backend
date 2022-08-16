511df8549868f663e54d4d148f99cd933a97d0c1
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-16 23:57:13 +0900

    [fix] recentrecord 수정

--- controllers/user.js
+++ controllers/user.js
@@ -308,7 +308,7 @@ async function recentRecord(req, res) {
             data.deaths = myData[0].deaths
             data.assists = myData[0].assists
             if (myData[0].deaths === 0) {
-                data.kda == Infinity
+                data.kda = Infinity
             } else {
                 data.kda = (myData[0].kills + myData[0].assists) / myData[0].deaths
             }
