a3c7dc584b8c2c5d5672cde9a18c4300d0d7a37a
DongYoung-dev <86117661+DongYoung-dev@users.noreply.github.com>
2022-08-27 15:35:26 +0900

    [feat] improvement schema 추가

--- schemas/chatroom.js
+++ schemas/chatroom.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const chatroomSchema = mongoose.Schema({ userId: { type: [String] } })
+const chatroomSchema = mongoose.Schema(
+    {
+        userId: { type: [String] } 
+    }
+)
 
 const ChatRoom = mongoose.model('Chatroom', chatroomSchema)
 module.exports = ChatRoom
---
+++ schemas/improvement.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const improvementSchema = mongoose.Schema(
+    {
+        context: { type: String }
+    }
+)
+
+const Improvement = mongoose.model('Improvement', improvementSchema)
+module.exports = Improvement
